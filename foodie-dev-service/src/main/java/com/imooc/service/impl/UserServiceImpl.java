3552c12464f002249984d1b4a7230ea8d194669d
aaaaa <969064814@qq.com>
2021-01-22 13:58:54 +0800

    1.21

--- foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -64,4 +64,16 @@ public class UserServiceImpl implements UserService {
         usersMapper.insert(users);
         return users;
     }
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Users queryUserForLogin(String username, String password) {
+        Example userExample = new Example(Users.class);
+        Example.Criteria userCriteria = userExample.createCriteria();
+        userCriteria.andEqualTo("username", username);
+        userCriteria.andEqualTo("password", password);
+
+        Users result = usersMapper.selectOneByExample(userExample);
+
+        return result;
+    }
 }
